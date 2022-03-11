ef6d067a121f147bd41e339e3700e8767d5f8554
AigerimToktosunova <100197282+AigerimToktosunova@users.noreply.github.com>
2022-03-10 19:34:19 -1100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// "use strict";
+ 
+// console.log (typeof(String(null)));
+
+// console.log (String(null));
+
+// console.log (typeof(String(5)));
+
+// console.log (typeof(null+ ''));
+
+// const num = 5;
+// console.log('https://vc.com/catalog/' + num);
+
+// const font = 25 + 'px';
+
+// // ...............1...........
+
+// console.log(typeof(Number('5')));
+
+// // ...............2...........
+
+// console.log(typeof(+ '5'));
+
+// // ...............2...........
+
+// console.log(typeof(parseInt("15px", 10)));
+
+
+
+// // let ans = +prompt ("Hello",  "");
+// // console.log(ans);
+
+// // to Boolean
+
+// // 0, '', null, undefined, Nan; pustoi znache.ia chygarbait
+
+// let switcher = null;
+//  if (switcher){
+//          console.log('working...')
+//  }
+
+//  switcher=1;
+//   if (switcher){
+//           console.log('working...')
+//   }
+
+// //   2 var.t Logicheskiytip
+
+// console.log(typeof(Boolean('s')));
+
+// //   2 var.t staryi
+
+// console.log(typeof(!! "1234"));
